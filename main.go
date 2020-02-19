b38b96f11021286b81fa790aaf73f1d5f63ee392
kubesure <edakghar@gmail.com>
2020-02-19 22:13:48 +0400

    initial commit

---
+++ main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+
+	log "github.com/sirupsen/logrus"
+)
+
+func init() {
+	log.SetFormatter(&log.JSONFormatter{})
+	log.SetLevel(log.DebugLevel)
+	log.SetOutput(os.Stdout)
+	log.SetReportCaller(true)
+}
+
+func main() {
+	/* file, err := os.OpenFile("sidecar-security.log", os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.SetOutput(os.Stdout)
+	} else {
+		log.SetOutput(file)
+		defer file.Close()
+	} */
+	log.Info("sidecar security starting...")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", invoke)
+	srv := http.Server{Addr: ":8000", Handler: mux}
+	ctx := context.Background()
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	go func() {
+		for range c {
+			log.Info("shutting down sidecar security...")
+			srv.Shutdown(ctx)
+			<-ctx.Done()
+		}
+	}()
+	log.Info("security sidecar started...")
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+		log.Fatalf("ListenAndServe(): %s", err)
+	}
+}
+
+func invoke(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(200)
+	data := (time.Now()).String()
+	log.Debug("Invoked....")
+	w.Write([]byte(data))
+}
