3c9b9da7a623c7baff934ff5fda7af0e37241f63
kubesure <edakghar@gmail.com>
2020-03-04 23:13:25 +0400

    connecting to TCP fraud checker service

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -1,6 +1,7 @@
 package middleware
 
 import (
+	"net"
 	"net/http"
 	"net/http/httputil"
 	"os"
@@ -14,7 +15,20 @@ func init() {
 	log.SetLevel(log.InfoLevel)
 }
 
-//Logger middleware function logs request params
+//FraudCheck message is sent to fraud checker to run fraud checks on the request.
+type FraudCheck struct {
+	smhSegIDVersion string
+	smhMsgVersion   string
+	smhTranType     string
+	smhCustType     string
+	smhActType      string
+	smhSource       string
+	fromAccount     string
+	clientIP        string
+	customerID      string
+}
+
+//Logger middleware logs orgin's request
 func Logger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Infof("logging: request middleware")
@@ -23,10 +37,10 @@ func Logger(next http.Handler) http.Handler {
 	})
 }
 
-//Auth middleware function authenticates request
+//Auth middleware authenticates request
 func Auth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Infof("Auth: Request before proxy call")
+		//log.Infof("Authenticating request: %v", r)
 		if r.Header.Get("user") != "foo" {
 			w.WriteHeader(http.StatusUnauthorized)
 			return
@@ -36,10 +50,56 @@ func Auth(next http.Handler) http.Handler {
 	})
 }
 
-//Final middleware
+//Final middleware forwards request to orgin
 func Final(proxy *httputil.ReverseProxy) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Println("Passing call to origin")
 		proxy.ServeHTTP(w, r)
 	})
+}
+
+//FraudChecker checks if the request is fradulent.
+func FraudChecker(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Infof("Fraud checking request...")
+		msg, msgerr := makeTCPMessage(r)
+		if msgerr != nil {
+			log.Errorf("Erorr while generating message %v", msgerr)
+		}
+
+		conn, cerr := net.Dial("tcp", ":8080")
+		defer conn.Close()
+		if cerr != nil {
+			log.Errorf("Error while connecting to Fraud Server")
+		}
+
+		_, werr := conn.Write([]byte(*msg))
+		if werr != nil {
+			log.Errorf("Error while sending message to TCP server %v", werr)
+		}
+
+		reply := make([]byte, 1024)
+		tcpmsg, rerr := conn.Read(reply)
+		if rerr != nil {
+			log.Errorf("Error while sending message to TCP server %v", rerr)
+			return
+		}
+		log.Printf("EOF message received")
+		log.Printf(string(tcpmsg))
+		next.ServeHTTP(w, r)
+
+		/*res := bufio.NewReader(conn)
+		tcpmsg, rerr := res.ReadBytes(byte('\n'))
+		if rerr == io.EOF {
+			log.Printf("EOF message received")
+			log.Printf(string(tcpmsg))
+			next.ServeHTTP(w, r)
+			return
+		}*/
+	})
+}
+
+func makeTCPMessage(r *http.Request) (*string, error) {
+	message := string("smh_seg_id_version:000004|smh_source:")
+	return &message, nil
 }
